03bc56cc79570febee79d2037234cedf5f4694e5
Nicolas Alvarado <na.alvarado@uniandes.edu.co>
2021-11-05 16:46:46 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# SafeSpots
+
+Esta es la app de SafeSpots desarrollada para el curso de PMC.
---
+++ safespots/android/app/src/main/kotlin/com/example/safespots/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.safespots
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
